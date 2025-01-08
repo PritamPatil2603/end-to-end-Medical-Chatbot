6fbfbedaacc61eee58307d9650114e6b56474316
Tanaji Patil <pritam.tanaji.patil@accenture.com>
2025-01-08 14:40:58 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name  =  'End to end Medical chatbot',
+    version = '0.0.0',
+    author = 'Pritam Patil',
+    author_email = 'pritam1998patil@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
